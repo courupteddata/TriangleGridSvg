f7ebc79e336c8b05376b435060244993e19b0eed
Nathan <courupteddata@users.noreply.github.com>
2019-04-26 23:06:47 -0400

    Add files via upload
    Some code cleanup.
    I was not happy with the general state, despite this being a small demo.

--- script.js
+++ script.js
@@ -1,41 +1,55 @@
 var svg = document.getElementById("theTriangleGrid");
 
+function createTri(id, points){
+  let tri = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+  tri.setAttribute("id", id);
+  /*
+  Although the standard allows commas inplace of spaces, your milage may vary and you many need
+  one of these.
+  Alternative 1:
+    let point_string = String(points[0][0]) + "," + String(points[0][1]) + " " +
+                       String(points[1][0]) + "," + String(points[1][1]) + " " +
+                       String(points[2][0]) + "," + String(points[2][1]);
+  
+  Since it's only a triangle I think it's worth just writing out the three pairs
+  instead of using a loop.
+  Alternative 2:
+    for (pair of points){
+      point_string += String(pair[0]) + "," + String(pair[1]) + " ";
+    }
+  */
+  tri.setAttribute("points", points);
+  return tri;
+}
+
 function createSquare(centerPointx, centerPointy, size, id, svg){
-  const half_size = size / 2;
+  const HALF_SIZE = size / 2;
+  const TRIS = ["_top", "_bottom", "_left", "_right"]
 
-  var top_tri = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-  top_tri.setAttribute("id", id + "_top");
-  var points = String(centerPointx) + "," + String(centerPointy) + " " + String(centerPointx - half_size) + "," + String(centerPointy - half_size) + " " + String(centerPointx + half_size) + "," + String(centerPointy - half_size);
-  top_tri.setAttribute("points", points);
+  let points = [[centerPointx, centerPointy],
+                [centerPointx - HALF_SIZE, centerPointy - HALF_SIZE],
+                [centerPointx + HALF_SIZE, centerPointy - HALF_SIZE]];
+  svg.appendChild(createTri(id + TRIS[0], points))
 
-  svg.appendChild(top_tri);
+  points = [[centerPointx,centerPointy],
+            [centerPointx - HALF_SIZE, centerPointy + HALF_SIZE], 
+            [centerPointx + HALF_SIZE, centerPointy + HALF_SIZE]];
+  svg.appendChild(createTri(id + TRIS[1], points))
 
-  var bottom_tri = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-  bottom_tri.setAttribute("id", id + "_bottom");
-  var points = String(centerPointx) + "," + String(centerPointy) + " " + String(centerPointx - half_size) + "," + String(centerPointy + half_size) + " " + String(centerPointx + half_size) + "," + String(centerPointy + half_size);
-  bottom_tri.setAttribute("points", points);
+  points = [[centerPointx, centerPointy],
+            [centerPointx - HALF_SIZE, centerPointy + HALF_SIZE],
+            [centerPointx - HALF_SIZE, centerPointy - HALF_SIZE]];
+  svg.appendChild(createTri(id + TRIS[2], points))
 
-  svg.appendChild(bottom_tri);
-
-  var left_tri = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-  left_tri.setAttribute("id", id + "_left");
-  points = String(centerPointx) + "," + String(centerPointy) + " " + String(centerPointx - half_size) + "," + String(centerPointy + half_size) + " " + String(centerPointx - half_size) + "," + String(centerPointy - half_size);
-  left_tri.setAttribute("points", points);
-
-  svg.appendChild(left_tri);
-
-
-  var right_tri = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-  right_tri.setAttribute("id", id + "_left");
-  points = String(centerPointx) + "," + String(centerPointy) + " " + String(centerPointx + half_size) + "," + String(centerPointy + half_size) + " " + String(centerPointx + half_size) + "," + String(centerPointy - half_size);
-  right_tri.setAttribute("points", points);
-
-  svg.appendChild(right_tri);
+  points = [[centerPointx, centerPointy],
+            [centerPointx + HALF_SIZE, centerPointy + HALF_SIZE],
+            [centerPointx + HALF_SIZE, centerPointy - HALF_SIZE]];
+  svg.appendChild(createTri(id + TRIS[3], points))
 }
 
 function createGrid(num_rows, num_cols, square_size) {
   for(let x = 0; x < num_cols; x++) {
-    for(let y= 0; y < num_rows; y++) {
+    for(let y = 0; y < num_rows; y++) {
       createSquare(x * square_size + square_size, y * square_size + square_size, square_size, "test" + x + "_" + y, svg);
     }
   }
