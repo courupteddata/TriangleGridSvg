c0b23004004dc346e2daee63dbcc5d3a38b1ec4b
Nathan <courupteddata@users.noreply.github.com>
2019-05-04 17:03:39 -0400

    Update
    Made it easier to create the grid with varying grid sizes.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var svg = document.getElementById("theTriangleGrid");
+var globalSVG = document.getElementById("theTriangleGrid");
 
-function createTri(id, points){
+function createTriangle(id, points){
   let tri = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
   tri.setAttribute("id", id);
   /*
@@ -20,40 +20,60 @@ function createTri(id, points){
   */
   tri.setAttribute("points", points);
   return tri;
-}
+};
 
-function createSquare(centerPointx, centerPointy, size, id, svg){
+function createSquare(svg, centerPointx, centerPointy, size, id, svg){
   const HALF_SIZE = size / 2;
   const TRIS = ["_top", "_bottom", "_left", "_right"]
 
   let points = [[centerPointx, centerPointy],
                 [centerPointx - HALF_SIZE, centerPointy - HALF_SIZE],
                 [centerPointx + HALF_SIZE, centerPointy - HALF_SIZE]];
-  svg.appendChild(createTri(id + TRIS[0], points))
+  svg.appendChild(createTriangle(id + TRIS[0], points))
 
   points = [[centerPointx,centerPointy],
             [centerPointx - HALF_SIZE, centerPointy + HALF_SIZE], 
             [centerPointx + HALF_SIZE, centerPointy + HALF_SIZE]];
-  svg.appendChild(createTri(id + TRIS[1], points))
+  svg.appendChild(createTriangle(id + TRIS[1], points))
 
   points = [[centerPointx, centerPointy],
             [centerPointx - HALF_SIZE, centerPointy + HALF_SIZE],
             [centerPointx - HALF_SIZE, centerPointy - HALF_SIZE]];
-  svg.appendChild(createTri(id + TRIS[2], points))
+  svg.appendChild(createTriangle(id + TRIS[2], points))
 
   points = [[centerPointx, centerPointy],
             [centerPointx + HALF_SIZE, centerPointy + HALF_SIZE],
             [centerPointx + HALF_SIZE, centerPointy - HALF_SIZE]];
-  svg.appendChild(createTri(id + TRIS[3], points))
-}
+  svg.appendChild(createTriangle(id + TRIS[3], points))
+};
 
-function createGrid(num_rows, num_cols, square_size) {
+function createGrid(svg, num_rows, num_cols, square_size) {
   for(let x = 0; x < num_cols; x++) {
     for(let y = 0; y < num_rows; y++) {
-      createSquare(x * square_size + square_size, y * square_size + square_size, square_size, "test" + x + "_" + y, svg);
+      createSquare(svg, 
+        x * square_size + square_size, 
+        y * square_size + square_size, 
+        square_size, 
+        "test_" + x + "_" + y, svg);
     }
   }
 };
 
-createGrid(100, 100, 40);
+const WIDTH = 1000; //In Pixels
+const HEIGHT = 700; //In Pixels
+const SQUARE_SIZE = 40; //In Pixels
+
+const HALF_SQUARE_SIZE = 0.5 * SQUARE_SIZE;
+
+//Make sure that the width and height are a multiple of the square size.
+const ADJUSTED_WIDTH = WIDTH <= 0 ? SQUARE_SIZE : SQUARE_SIZE * Math.ceil(WIDTH / SQUARE_SIZE);
+const ADJUSTED_HEIGHT = HEIGHT <= 0 ? SQUARE_SIZE : SQUARE_SIZE * Math.ceil(HEIGHT / SQUARE_SIZE);
+
+const NUM_COLS = ADJUSTED_WIDTH / SQUARE_SIZE;
+const NUM_ROWS = ADJUSTED_HEIGHT / SQUARE_SIZE;
+
+globalSVG.setAttribute("height", ADJUSTED_HEIGHT + HALF_SQUARE_SIZE);
+globalSVG.setAttribute("width", ADJUSTED_WIDTH + HALF_SQUARE_SIZE);
+
+createGrid(globalSVG, NUM_ROWS, NUM_COLS, SQUARE_SIZE);
 
